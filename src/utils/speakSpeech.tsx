1af2db7ec36cbb158cbe0eb5cc7474d3fb188c6a
super nova <teozic@proton.me>
2025-05-11 17:38:30 +0300

    speakSpeech function is added

---
+++ src/utils/speakSpeech.tsx
@@ -0,0 +1,6 @@
+export const speakText = (text: string) => {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'en-US';
+    synth.speak(utterance);
+  };
